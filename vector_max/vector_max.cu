50d952d8ee1386b549a54188f547d189cc9bea92
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2022-04-10 17:14:08 +0800

    Added vector_max fortran implementation.

--- vector_max/vector_max.cu
+++ vector_max/vector_max.cu
@@ -6,12 +6,16 @@
 #define NTHREADS 1024
 
 
+void __cuda_check_error(cudaError_t err, const char *file, int line){
+	if(err != cudaSuccess){
+        fprintf(stderr, "CUDA error (%s:%d): %s\n", file, line, cudaGetErrorString(err));
+        exit(1);
+    }
+}
+
 
 #define CUDA_CHECK_ERROR(X)({\
-    if((X) != cudaSuccess){\
-        fprintf(stderr, "CUDA error (%s:%d): %s\n", __FILE__, __LINE__, cudaGetErrorString((X)));\
-        exit(1);\
-    }\
+	__cuda_check_error((X), __FILE__, __LINE__);\
 })
 
 
@@ -27,9 +31,9 @@
 
 __global__ void vector_max(int *v, int *max, int n){
     unsigned int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    unsigned int lane_id = threadIdx.x % 32;
-    unsigned int warp_id = threadIdx.x / 32;
-    __shared__ int block_max[32];
+    unsigned int lane_id = threadIdx.x % warpSize;
+    unsigned int warp_id = threadIdx.x / warpSize;
+    __shared__ int block_max[warpSize];
     if (idx < n && lane_id == 0) block_max[warp_id] = v[idx];
     __syncthreads();
    // TODO: continue here
@@ -46,17 +50,23 @@ int vector_max_driver(int *v, int n){
     CUDA_CHECK_ERROR(cudaMemcpy(dev_max, v, sizeof(int), cudaMemcpyHostToDevice));
     // setup kernel configuration
     unsigned int nBlocks = (n + NTHREADS - 1) / NTHREADS;
-    ptimer_t kernel_timer;
-    timer_start(&kernel_timer);
+    cudaEvent_t start, stop;
+    CUDA_CHECK_ERROR(cudaEventCreate(&start));
+    CUDA_CHECK_ERROR(cudaEventCreate(&stop));
+    CUDA_CHECK_ERROR(cudaEventRecord(start));
     vector_max<<<nBlocks, NTHREADS>>>(dev_v, dev_max, n);
     CUDA_CHECK_ERROR(cudaGetLastError());
+    CUDA_CHECK_ERROR(cudaEventRecord(stop));
     CUDA_CHECK_ERROR(cudaDeviceSynchronize());
-    timer_stop(&kernel_timer);
-    printf("'vector_max' kernel execution time (ms): %.3lf\n", timer_elapsed(kernel_timer));
+    float elapsed;
+    CUDA_CHECK_ERROR(cudaEventElapsedTime(&elapsed, start, stop));
+    printf("'vector_max' kernel execution time (ms): %.3lf\n", elapsed);
     CUDA_CHECK_ERROR(cudaMemcpy(&max, dev_max, sizeof(int), cudaMemcpyDeviceToHost));
     CUDA_CHECK_ERROR(cudaDeviceSynchronize());
     CUDA_CHECK_ERROR(cudaFree(dev_v));
     CUDA_CHECK_ERROR(cudaFree(dev_max));
+    CUDA_CHECK_ERROR(cudaEventDestroy(start));
+    CUDA_CHECK_ERROR(cudaEventDestroy(stop));
     return max;
 }
 
