05d5793f970ee768ce292b8346ddc81b65751596
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2022-04-10 23:04:39 +0800

    Reviewed vector_add examples.

--- vector_add/gpu_vector_add.cu
+++ vector_add/gpu_vector_add.cu
@@ -43,16 +43,15 @@ void init_vec(float *v, int n){
 
 
 // kernel to perform vector addition
-__global__ void vector_add(float *a, float *b, float *c, int n){
-    unsigned int i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i < n)
-        c[i] = a[i] + b[i];
+__global__ void vector_add(float *a, float *b, float *c){
+    unsigned int i = threadIdx.x;
+    c[i] = a[i] + b[i];
 }
 
 
 
 int main(void){
-    const int n = 2000;
+    const unsigned int n = 100;
     float *A = (float*) malloc(n * sizeof(float));
     float *B = (float*) malloc(n * sizeof(float));
     float *C = (float*) malloc(n * sizeof(float));
@@ -65,26 +64,20 @@ int main(void){
     CUDA_CHECK_ERROR(cudaMalloc(&dev_C, sizeof(float) * n));
     CUDA_CHECK_ERROR(cudaMemcpy(dev_A, A, sizeof(float) * n, cudaMemcpyHostToDevice));
     CUDA_CHECK_ERROR(cudaMemcpy(dev_B, B, sizeof(float) * n, cudaMemcpyHostToDevice));
-    const int nBlocks = (n + NTHREADS - 1) / NTHREADS;
-    vector_add<<<nBlocks, NTHREADS>>>(dev_A, dev_B, dev_C, n);
-    CUDA_CHECK_ERROR((cudaError_t)cudaGetLastError());
-    CUDA_CHECK_ERROR(cudaDeviceSynchronize());
+    
+    vector_add<<<1, n>>>(dev_A, dev_B, dev_C);
+
+    CUDA_CHECK_ERROR(cudaGetLastError());
+    // some errors happened during kernel execution may show up only after device synchronization
+    CUDA_CHECK_ERROR(cudaDeviceSynchronize()); 
     CUDA_CHECK_ERROR(cudaMemcpy(C, dev_C, sizeof(float) * n, cudaMemcpyDeviceToHost));
     CUDA_CHECK_ERROR(cudaDeviceSynchronize());
     
     // check the result is correct
+    bool sums_equal = true;
     for(int i = 0; i < n; i++){
-        bool sums_equal = compare_float(C[i], A[i] + B[i]);
-        if(!sums_equal){
-            fprintf(stderr, "Sum is not correct.\n");
-            cudaFree(dev_A);
-            cudaFree(dev_B);
-            cudaFree(dev_C);
-            free(A);
-            free(B);
-            free(C);
-            return 1;
-        }
+        sums_equal = compare_float(C[i], A[i] + B[i]);
+        if(!sums_equal) break;
     }
     CUDA_CHECK_ERROR(cudaFree(dev_A));
     CUDA_CHECK_ERROR(cudaFree(dev_B));
@@ -92,6 +85,12 @@ int main(void){
     free(A);
     free(B);
     free(C);
-    printf("All good.\n");
-    return 0;
+    if(sums_equal){
+        printf("All good.\n");
+        return EXIT_SUCCESS;
+    }else{
+        fprintf(stderr, "Sum is not correct.\n");
+        return EXIT_FAILURE;
+    }
+    
 }
--- vector_add/gpu_vector_add_perf.cu
+++ vector_add/gpu_vector_add_perf.cu
@@ -56,7 +56,7 @@ void cpu_vector_add(float *a, float *b, float *c, int n){
 
 
 // kernel to perform vector addition
-__global__ void vector_add(float *A, float *B, float *C, int n){
+__global__ void vector_add(float *A, float *B, float *C, unsigned int n){
     unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
     if(i < n)
         C[i] = A[i] + B[i];
@@ -64,14 +64,14 @@ __global__ void vector_add(float *A, float *B, float *C, int n){
 
 
 
-void vector_add_driver(float *A, float *B, float *C, int n){
+void vector_add_driver(float *A, float *B, float *C, unsigned int n){
     float *dev_A, *dev_B, *dev_C;
     CUDA_CHECK_ERROR(cudaMalloc(&dev_A, sizeof(float) * n));
     CUDA_CHECK_ERROR(cudaMalloc(&dev_B, sizeof(float) * n));
     CUDA_CHECK_ERROR(cudaMalloc(&dev_C, sizeof(float) * n));
     CUDA_CHECK_ERROR(cudaMemcpy(dev_A, A, sizeof(float) * n, cudaMemcpyHostToDevice));
     CUDA_CHECK_ERROR(cudaMemcpy(dev_B, B, sizeof(float) * n, cudaMemcpyHostToDevice));
-    const int nblocks = (n + NTHREADS - 1) / NTHREADS;
+    const unsigned int nblocks = (n + NTHREADS - 1) / NTHREADS;
     cudaEvent_t start, stop;
     CUDA_CHECK_ERROR(cudaEventCreate(&start));
     CUDA_CHECK_ERROR(cudaEventCreate(&stop));
@@ -95,7 +95,7 @@ void vector_add_driver(float *A, float *B, float *C, int n){
 
 
 void test_correctness(void){
-    int n = 100;
+    const unsigned int n = 100;
     float *A = (float*) malloc(n * sizeof(float));
     float *B = (float*) malloc(n * sizeof(float));
     float *C = (float*) malloc(n * sizeof(float));
@@ -109,7 +109,7 @@ void test_correctness(void){
     vector_add_driver(A,B, C, n);
     // test correctness of result
     char test_passed = 1;
-    for(int i = 0; i < n; i++){
+    for(unsigned int i = 0; i < n; i++){
         if(!compare_float(C[i], n)){
             test_passed = 0;
             break;
@@ -127,7 +127,7 @@ void test_correctness(void){
 
 
 
-void test_performance(int n){
+void test_performance(unsigned int n){
     float *A = (float*) malloc(n * sizeof(float));
     float *B = (float*) malloc(n * sizeof(float));
     float *C_gpu = (float*) malloc(n * sizeof(float));
@@ -160,7 +160,7 @@ void test_performance(int n){
     print_array_terse(C_cpu, n, 3);
     
     char test_passed = 1;
-    for(int i = 0; i < n; i++){
+    for(unsigned int i = 0; i < n; i++){
         if(!compare_float(C_cpu[i], C_gpu[i])){
             test_passed = 0;
             break;
@@ -178,9 +178,9 @@ void test_performance(int n){
 
 
 int main(int argc, char **argv){
-    int n = 100;
+    unsigned int n = 100;
     if(argc >= 2){
-        n = atoi(argv[1]);
+        n = (unsigned int) atoi(argv[1]);
     }else{
         printf("Using default n = 100 for performance testing.\nTo change behaviour, %s [n]\n\n", argv[0]);
     }
